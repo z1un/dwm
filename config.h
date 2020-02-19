d3426bc353769d463a89eb6b9336742a13158539
BestReder <997594560@qq.com>
2020-02-19 15:09:59 +0800

    updata

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#37474F";
-static const char col_border[]        = "#42A5F5";
+static const char col_border[]      = "#42A5F5";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
 static const char *i3lockcmd[]  = { "i3lock-fancy", NULL };
 static const char *browsercmd[]  = { "google-chrome-stable", NULL };
 static const char *screenshot[]  = { "deepin-screenshot", NULL };
